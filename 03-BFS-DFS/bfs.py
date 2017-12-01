c7625d9c383c1e72e9d5a4de3130ca24f60811fc
spooks <g4briel.4lves@gmail.com>
2017-12-01 15:17:12 -0200

    Finished BFS

--- 03-BFS-DFS/bfs.py
+++ 03-BFS-DFS/bfs.py
@@ -1,26 +1,39 @@
-import network as nx
+import networkx as nx
 import matplotlib.pyplot as plt
 
-def bfs(G, s):
+def bfs(graph, start):
     # Initialize all nodes to white color, infinite distance and no parent
-    nx.set_node_attributes(G, 'color', 'white')
-    nx.set_node_attributes(G, 'min_dist', float('inf'))
-    nx.set_node_attributes(G, 'parent', None)
+    for node in graph:
+        nx.set_node_attributes(graph, 'color', 'white')
+        nx.set_node_attributes(graph, 'min_dist', float('inf'))
+        nx.set_node_attributes(graph, 'parent', None)
 
     # Initialize starting node to gray color, 0 distance
-    nx.set_node_attributes(G, 'color', {s:'gray'})
-    nx.set_node_attributes(G, 'min_dist', {s:0})
+    
+    nx.set_node_attributes(graph, 'color', {start:'gray'})
+    nx.set_node_attributes(graph, 'min_dist', {start:0})
 
     # Initialize the queue and push the starting node into it
     Q = []
-    Q.append(s)
+    Q.append(start)
     while len(Q) > 0:
-        c = Q.pop()
-        for n in nx.all_neighbors(G, c):
-            if G[n]['color'] == 'white':
-                nx.set_node_attributes(G, 'min_dist', {n:(G[c]['min_dist'] + 1)})
-                nx.set_node_attributes(G, 'parent', {n:c})
-                nx.set_node_attributes(G, 'color', {n:'gray'})
-                Q.append(n)
-            nx.set_node_attributes(G, 'color', {c:'black'}
+        curr = Q.pop()
+        for nbr in nx.all_neighbors(graph, curr):
+            if graph.node[nbr]['color'] == 'white':
+                nx.set_node_attributes(graph, 'min_dist', {nbr:graph.node[curr]['min_dist'] + 1})
+                nx.set_node_attributes(graph, 'color', {nbr:'gray'})
+                nx.set_node_attributes(graph, 'parent', {nbr:curr})
+                Q.append(nbr)
+        nx.set_node_attributes(graph, 'color', {curr:'black'})
+    
+    # Build the resulting tree
+    tree = nx.DiGraph()
 
+    # Insert starting node
+    tree.add_node(start)
+
+    # Insert remaining nodes
+    for n in graph:
+        tree.add_edge(graph.node[n]['parent'], n)
+
+    return tree
--- 03-BFS-DFS/main.py
+++ 03-BFS-DFS/main.py
@@ -1,39 +1,31 @@
 import networkx as nx
 import pygraphviz
 import matplotlib.pyplot as plt
-#from bfs import bfs
+from bfs import bfs
 #from dfs import dfs
 
-# Load Graphs from .paj
+# Load graphs from .paj
 G_karate = nx.read_pajek('karate.paj')
 G_dolphins = nx.read_pajek('dolphins.paj')
 
+# Generate BFS and DFS for both graphs using NetworkX's methods on the first node of each
+nx_karate_bfs = [nx.bfs_tree(G_karate, '1'), "karate_bfs_nx.dot"]
+nx_karate_dfs = [nx.dfs_tree(G_karate, '1'), "karate_dfs_nx.dot"]
+nx_dolphins_bfs = [nx.bfs_tree(G_dolphins, '0'), "dolphins_bfs_nx.dot"]
+nx_dolphins_dfs = [nx.dfs_tree(G_dolphins, '0'), "dolphins_dfs_nx.dot"]
+
+# Generate BFS and DFS using our methods on the first node of each
+our_karate_bfs = [bfs(G_karate, '1'), "karate_bfs_our.dot"]
+our_dolphins_bfs = [bfs(G_dolphins, '0'), "dolphins_bfs_our.dot"]
+
 # Store NetworkX's results in a tuple for later comparing
-NX_res_karate = []
-NX_res_dolphins = []
-NX_results = (NX_res_karate, NX_res_dolphins)
+NX_results = (nx_karate_bfs, nx_karate_dfs, nx_dolphins_bfs, nx_dolphins_dfs)
 
-# Generate BFS and DFS for both Graphs using NetworkX's methods on the first Node of each
-karate_bfs = nx.bfs_tree(G_karate, '1')
-karate_dfs = nx.dfs_tree(G_karate, '1')
-
-dolphins_bfs = nx.bfs_tree(G_dolphins, '0')
-dolphins_dfs = nx.dfs_tree(G_dolphins, '0')
-
-# Store results in each Graph's list
-NX_res_karate.append(karate_bfs)
-NX_res_karate.append(karate_dfs)
-
-NX_res_dolphins.append(dolphins_bfs)
-NX_res_dolphins.append(dolphins_dfs)
+# Store our results in another tuple
+Our_results = (our_karate_bfs, our_dolphins_bfs)
 
 # Save results in AGraph Dot format
-
-path = "karate_bfs_nx.dot"
-nx.drawing.nx_agraph.write_dot(NX_results[0][0], path)
-path = "karate_dfs_nx.dot"
-nx.drawing.nx_agraph.write_dot(NX_results[0][1], path)
-path = "dolphins_bfs_nx.dot"
-nx.drawing.nx_agraph.write_dot(NX_results[1][0], path)
-path = "dolphins_dfs_nx.dot"
-nx.drawing.nx_agraph.write_dot(NX_results[1][1], path)
+for graph in NX_results:
+    nx.drawing.nx_agraph.write_dot(graph[0], graph[1])
+for graph in Our_results:
+    nx.drawing.nx_agraph.write_dot(graph[0], graph[1])
